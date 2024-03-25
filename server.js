3bf50413ee862288b4bf62a85e5bce98a2fe6b70
D'yara Williams <dyara.williams@gmail.com>
2024-03-24 20:54:34 -0400

    completed code

--- server.js
+++ server.js
@@ -15,7 +15,54 @@ app.get('/', (req, res) => {
     res.render('index', { records: null })
 })
 
-
+app.get('/search', async (req, res) => {
+    const { lastName, firstName, birthYear, deathYear } = req.query;
+  
+    let query = supabase
+        .from('grave_register')
+        .select(`
+            name_last,
+            name_maiden,
+            name_first,
+            name_middle,
+            title,
+            birth_date,
+            death_date,
+            age,
+            is_veteran,
+            section,
+            lot,
+            moved_from,
+            moved_to_lot,
+            notes
+        `)
+        .order('name_last', { ascending: true });
+  
+    if (lastName) {
+        query = query.ilike('name_last', `%${lastName}%`);
+    }
+    if (firstName) {
+        query = query.ilike('name_first', `%${firstName}%`);
+    }
+    if (birthYear) {
+        query = query.eq('birth_year', birthYear);
+    }
+    if (deathYear) {
+        query = query.eq('death_year', deathYear);
+    }
+  
+    try {
+        let { data, error } = await query;
+        if (error) {
+            throw error;
+        }
+        // console.log(data)
+        res.render('index', { records: data });
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+  })
 
 app.listen(PORT || process.env.PORT, () => {
     console.log(`Server running on port ${PORT}`);
